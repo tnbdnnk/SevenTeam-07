d12acdd21df02e3bd2fcf7807e210766564c485b
Tatiana Bidnenko <tnbdnnk@gmail.com>
2024-05-02 17:51:16 +0300

    Fix

--- src/components/Sidebar/BoardList/BoardList.jsx
+++
@@ -1,7 +0,0 @@
-const BoardList = ({ items }) => {
-  const list = items.map(item => { return (<li key={item.id}>{item.name}</li>)})
-
-  return <ul>Тут буде список бордів: {list}</ul>
-}
-
-export default BoardList
