116dcd5d44f0d960c8f86d9882a2d5d5f775a1d9
Tatiana Bidnenko <tnbdnnk@gmail.com>
2024-05-02 10:20:20 +0300

    Initial commit.

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,14 @@ import react from "@vitejs/plugin-react-swc";
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-      plugins: [react(), svgr()],
-      resolve: {
+    plugins: [react(), svgr()],
+    resolve: {
         alias: {
-          src: '/src',
-          components: '/src/components',
-          pages: '/src/pages',
-          assets: '/src/assets'
+        src: '/src',
+        components: '/src/components',
+        pages: '/src/pages',
+        assets: '/src/assets',
         },
-      },
-      base: '/react_vite/',
-    });
+    },
+    base: '/taskpro-team7-front/',
+});
