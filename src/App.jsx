606a8e510adfa33ab90e2acdd1c16857f30194f6
SerhiiMW <137871447+SerhiiMW@users.noreply.github.com>
2024-05-06 17:11:28 +0300

    auth_v2

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,12 @@ import SecondPage from 'pages/SecondPage/SecondPage';
 import HalfPage from 'pages/HalfPage/HalfPage';
 import ErrorPage from 'pages/ErrorPage/ErrorPage';
 
-// import RegisterPage from './pages/AuthPage/RegisterPage';
-// import LoginPage from './pages/AuthPage/LoginPage';
+import { lazy } from "react";
+const RegisterPage = lazy(() => import("./pages/AuthPage/RegisterPage"));
+const LoginPage = lazy(() => import("./pages/AuthPage/LoginPage"));
+const WelcomePage = lazy(() => import("./pages/WelcomePage/WelcomePage"));
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
+import PublicRoute from "./components/PublicRoute/PublicRoute";
 
 import { AppWrapper } from './App.styled';
 // import HomePage from './pages/HomePage/HomePage';
@@ -21,12 +25,17 @@ function App() {
         <Routes>
             <Route path="/" element={<SharedLayout />}>
                 {/* <Route path="/home" element={<HomePage />}/> */}
+                <Route element={<PublicRoute />}>
+                    <Route path="/welcome" element={<WelcomePage />}/>
+                    <Route path="auth/register" element={<RegisterPage />} />
+                    <Route path="auth/login" element={<LoginPage />} />
+                </Route>
+                <Route element={<PrivateRoute />}>
                 <Route path="/home/:boardName" element={<ScreensPage />}/>
-                {/* <Route path="/register" element={<RegisterPage />} />
-                <Route path="/login" element={<LoginPage />} /> */}
                 <Route path="/first" element={<FirstPage />} />
                 <Route path="/second" element={<SecondPage />}>
                     <Route path=":half" element={<HalfPage />} />
+                </Route>
                 </Route>
                 <Route path="*" element={<ErrorPage />} />
             </Route>
--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,15 +1,20 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Header } from 'components/Header/Header';
-// import { Welcome } from 'components/Welcome/Welcome'
+import Welcome  from 'components/Welcome/Welcome'
 
-// import Welcome from "../Welcome/Welcome";
+// import { useSelector } from "react-redux";
+
+// import { selectIsLogin } from "../../redux/auth/auth-selectors";
+
 
 const SharedLayout = () => {
+
   return (
     <>
       <Header />
-      {/* <Welcome /> */}
+      <Welcome />
+
       <Suspense fallback={null}>
         <Outlet />
       </Suspense>
@@ -17,17 +22,6 @@ const SharedLayout = () => {
   );
 };
 
-
-
-    // return (
-    //     <>
-    //         <Welcome />
-    //         <Suspense fallback={<p>...Loading page</p>}>
-    //             <Outlet />
-    //         </Suspense>
-    //     </>
-    // )
-// }
 
 
 export default SharedLayout;
