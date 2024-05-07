64776e1fcbd495736d30c84a6a17160160114c2d
Tatiana Bidnenko <tnbdnnk@gmail.com>
2024-05-07 11:08:41 +0300

    little fix needhelp, welcome

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -3,16 +3,16 @@ import styles from './welcome-page.module.css'
 import { selectIsLogin } from "../../redux/auth/auth-selectors";
 import WelcomeAuth from "../../components/Welcome/WelcomeAuth/WelcomeAuth";
 import WelcomeUser from "../../components/Welcome/WelcomeUser/WelcomeUser";
+
 const WelcomePage = ()=> {
     const isLogin = useSelector(selectIsLogin);
     return (
-        
         <div>
-        <h1>Task Pro</h1>
-        <p>Supercharge your productivity and take control of your tasks with Task Pro - Dont wait, start achieving your goals now!</p>
-        <nav className={styles.navbar}>
-        {isLogin ? <WelcomeUser /> : <WelcomeAuth />}   
-        </nav>
+            <h1>Task Pro</h1>
+            <p>Supercharge your productivity and take control of your tasks with Task Pro - Dont wait, start achieving your goals now!</p>
+            <nav className={styles.navbar}>
+                {isLogin ? <WelcomeUser /> : <WelcomeAuth />}   
+            </nav>
         </div>
     )
 }
