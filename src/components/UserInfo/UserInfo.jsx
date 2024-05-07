39a4bd1370d70ad631696e18ccfd29b00ca64616
AlMarina <almarina1017@gmail.com>
2024-05-07 19:14:01 +0300

    start

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -2,13 +2,17 @@ import css from '../UserInfo/UserInfo.module.css';
 // import { useState, useEffect } from 'react';
 import { useModal } from '../../hooks/useModal';
 import Modal from '../../helpers/ModalWindow/Modal';
-// import { UserInfoForm } from 'components/UserInfoForm/UserInfoForm';
+import { UserForm } from 'components/UserForm/UserForm';
 import sprite from '../../images/symbol-defs.svg';
+// import { useSelector } from 'react-redux';
+// import { selectUser } from '../../../redux/auth/auth-selectors';
 
 export const UserInfo = () => {
   // const avatarLight = `${sprite}#icon-user-icon-white-theme`;
   // const avatarViolet = `${sprite}#icon-user-icon-violet-theme`;
   const avatarDark = `${sprite}#icon-user-icon-dark-theme`;
+
+  // const { name, avatarURL } = useSelector(selectUser);
 
   // const [avatar, setAvatar] = useState(avatarDark);
 
@@ -16,15 +20,20 @@ export const UserInfo = () => {
 
   return (
     <div className={css.wrapUserInfo}>
-      <p className={css.textUser}>Name</p>
+      {/*   <p className={css.textUser}>{name ? name : 'Name'} </p> */}
+      <p className={css.textUser}>Name </p>
       <button className={css.avatarBtn} onClick={openModal}>
+        {/* {avatarURL ? (
+          <img className={css.avatar} src={avatarURL} alt="user-avatar" />
+        ) : ( */}
         <svg className={css.avatar}>
           <use href={avatarDark} />
         </svg>
-        {/* <img className={css.avatar} src={avatarViolet} alt="user-avatar" /> */}
+        {/* )} */}
       </button>
       <Modal isOpen={isModalOpen} onClose={closeModal}>
         {/* <UserForm avatar={avatar} onClose={closeModal} /> */}
+        <UserForm />
       </Modal>
     </div>
   );
