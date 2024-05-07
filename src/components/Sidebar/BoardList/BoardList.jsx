e829cdbec64402a1c62181ec73c43d358ec948df
GutMaria <gootmaria@gmail.com>
2024-05-07 15:24:40 +0300

    add boardList and editBoard

--- src/components/Sidebar/BoardList/BoardList.jsx
+++ src/components/Sidebar/BoardList/BoardList.jsx
@@ -1,9 +1,37 @@
+import { useState } from 'react';
+import { useModal } from "../../../hooks/useModal.js";
+import Modal from "../../../helpers/ModalWindow/Modal.jsx"
+import {EditBoard} from '../../../components/Sidebar/ModalBoards/ModalBoards.jsx'
+
 import css from "./boardList.module.css"
+import sprite from '../../../images/symbol-defs.svg'
 
 const BoardList = ({ items }) => {
-  const list = items.map(item => { return (<li key={item.id}>{item.name}</li>)})
 
-  return <ul className={css.list}>Тут буде список бордів: {list}</ul>
+  const [activeBoard, setActiveBoard] = useState(items[0]);
+  const handleBoardClick = (board) => {
+  setActiveBoard(board);
+  };
+
+    const { isModalOpen, openModal, closeModal } = useModal();
+
+  const list = items.map(item => {
+    return (<li key={item.id} className={activeBoard.id === item.id ? `${css.item} ${css.active}` : css.item}>
+      <button className={css.boardBtn} type='button' onClick={() => handleBoardClick(item)}>
+      <div className={css.boardInfo}>
+        <svg width="18" height="18" className={css.boardIcon}><use href={sprite + item.icons}></use></svg>
+        <h3>{item.title}</h3>
+      </div>
+      <div className={css.boarSettings}>
+          <span onClick={openModal}><svg className={css.boarSettingsSvg} width="16" height="16"><use href={sprite + '#icon-pen'}></use></svg></span>
+        <span><svg className={css.boarSettingsSvg} width="16" height="16"><use href={sprite + '#icon-trash'}></use></svg></span>
+      </div>
+      </button>
+      <Modal isOpen={isModalOpen} onClose={closeModal}>{<EditBoard boardData={activeBoard} />}</Modal>
+    </li>)
+  })
+
+  return <ul className={css.list}>{list}</ul>
 }
 
 export default BoardList
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -10,13 +10,14 @@ import {NewBoard} from './ModalBoards/ModalBoards.jsx'
 import css from './sidebar.module.css';
 import icons from '../../images/symbol-defs.svg';
 // Брати потім з бека
-const exampleBoardList = [{ id: 1, name: "First board"}, { id: 2, name: "Second board"}, ]
+const exampleBoardList = [{ id: 1, title: "First board", icons:"#icon-four-balls"}, { id: 2, title: "Second board", icons:'#icon-puzzle'}, { id: 3, title: "Third board", icons:"#icon-star"}, ]
 
 const Sidebar = ({ className }) => {
 // для модалки
   const { isModalOpen, openModal, closeModal } = useModal();
 
   return <aside className={`${css.sidebar} ${className}`}>
+    <div className={css.partSidebar}>
     <LogoComponent />
     <h3 className={css.text}>My boards</h3>
     <div className={css.createBox}>
@@ -25,14 +26,16 @@ const Sidebar = ({ className }) => {
       <svg className={css.iconPlus} width="20" height="20">
       <use href={icons + '#icon-plus'}></use>
     </svg></button></div>
-
+    </div>
     <Modal isOpen={isModalOpen} onClose={closeModal}>{ <NewBoard/>}</Modal>
 
     <BoardList items={exampleBoardList} />
+    <div className={css.partSidebar}>
     <NeedHelp />
     <button className={css.logoutBtn}>
       <svg className={css.iconLogout} width="32" height="32"><use href={icons + '#icon-logout'}></use></svg>
-      Log out</button>
+        Log out</button>
+    </div>
   </aside>
 }
 
