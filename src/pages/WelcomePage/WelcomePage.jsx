5c707d0c80bc0fe0d5a071207ff53edb0b558aff
Tatiana Bidnenko <tnbdnnk@gmail.com>
2024-05-09 15:53:43 +0300

    add welcome page emoji

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -3,38 +3,38 @@ import styles from './welcome-page.module.css';
 import { selectIsLogin } from '../../redux/auth/auth-selectors';
 import WelcomeAuth from '../../components/Welcome/WelcomeAuth/WelcomeAuth';
 import WelcomeUser from '../../components/Welcome/WelcomeUser/WelcomeUser';
-
+import welcomepageemoji from '/src/images/welcompage-emoji.svg';
 
 const WelcomePage = () => {
-  const isLogin = useSelector(selectIsLogin);
-  return (
-      <div className={styles.welcomeWrapper}>
-        <div className={styles.welcomeTitleWrapper}>
-          <img
-            src="../../images/ImageWelcome.png"
-            alt=""
-            className={styles.welcomeImg}
-            width="124"
-            height="124"
-          />
-          <div className={styles.welcomeSvgWrapper}>
-            <svg className={styles.welcomeSvg} width="40" height="40">
-              <use
-                className={styles.welcomeUse}
-                href="../../images/iconWelcome.svg"
-              ></use>
-            </svg>
-            <h1 className={styles.welcomeTitle}>Task Pro</h1>
-          </div>
+    const isLogin = useSelector(selectIsLogin);
+    return (
+        <div className={styles.welcomeWrapper}>
+            <div className={styles.welcomeTitleWrapper}>
+            <img
+                src={welcomepageemoji}
+                alt=""
+                className={styles.welcomeImg}
+                width="124"
+                height="124"
+            />
+            <div className={styles.welcomeSvgWrapper}>
+                <svg className={styles.welcomeSvg} width="40" height="40">
+                <use
+                    className={styles.welcomeUse}
+                    href="../../images/iconWelcome.svg"
+                ></use>
+                </svg>
+                <h1 className={styles.welcomeTitle}>Task Pro</h1>
+            </div>
+            </div>
+            <p className={styles.welcomeText}>
+            Supercharge your productivity and take control of your tasks with Task
+            Pro - Dont wait, start achieving your goals now!
+            </p>
+            <nav className={styles.navbar}>
+            {isLogin ? <WelcomeUser /> : <WelcomeAuth />}
+            </nav>
         </div>
-        <p className={styles.welcomeText}>
-          Supercharge your productivity and take control of your tasks with Task
-          Pro - Dont wait, start achieving your goals now!
-        </p>
-        <nav className={styles.navbar}>
-          {isLogin ? <WelcomeUser /> : <WelcomeAuth />}
-        </nav>
-      </div>
-  );
+    );
 };
 export default WelcomePage;
