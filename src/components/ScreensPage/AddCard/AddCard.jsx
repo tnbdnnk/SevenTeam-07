bcac2823980faf73504645bc41e94db93b05b85e
AnnaFedorovaGit <ann.petrovska94@gmail.com>
2024-05-13 09:24:42 +0300

    add themes

--- src/components/ScreensPage/AddCard/AddCard.jsx
+++ src/components/ScreensPage/AddCard/AddCard.jsx
@@ -3,7 +3,13 @@ import icons from '../../../images/symbol-defs.svg';
 import AddCardModal from './AddCardModal/AddCardModal';
 import { useModal } from '../../../hooks/useModal';
 
+import { useSelector } from 'react-redux';
+import { selectUser } from '../../../redux/auth/auth-selectors';
+
+
 const AddCard = ({ onAddCard, columnId }) => {
+  const { theme } = useSelector(selectUser);
+  
   const {
     openModal: openAddModal,
     closeModal: closeAddModal,
@@ -20,13 +26,13 @@ const AddCard = ({ onAddCard, columnId }) => {
           columnId={columnId}
         />
       )}
-      <button className={css.button} type="button" onClick={openAddModal}>
-        <div className={css.iconBox}>
-          <svg className={css.icon} width="14" height="14">
+      <button className={[css.button, css[theme]].join(' ')} type="button" onClick={openAddModal}>
+        <div className={[css.iconBox, css[theme]].join(' ')}>
+          <svg className={[css.icon, css[theme]].join(' ')} width="14" height="14">
             <use href={icons + '#icon-plus'}></use>
           </svg>
         </div>
-        <p className={css.text}>Add another card</p>
+        <p className={[css.text, css[theme]].join(' ')}>Add another card</p>
       </button>
     </>
   );
