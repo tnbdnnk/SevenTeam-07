5f094e4b1ece52dd69c0c3d9b6d54d0168da7d0d
Tatiana Bidnenko <tnbdnnk@gmail.com>
2024-05-14 20:11:38 +0300

    add css need help modal

--- src/components/Sidebar/NeedHelp/NeedHelp.jsx
+++ src/components/Sidebar/NeedHelp/NeedHelp.jsx
@@ -12,7 +12,12 @@ const NeedHelp = () => {
   return <div className={[css.needHelpBox, css[theme]].join(' ')}>
     <div className={css.cactusBox}></div>
     <p>If you need help with <br /> <span>TaskPro</span>, check out our support resources or reach out to our customer support team</p>
-    <button onClick={openModal} className={css.btn}><svg className={css.icon} width="20" height="20"><use href={icons + '#icon-help-circle'}></use></svg>Need help?</button>
+    <button onClick={openModal} className={css.btn}>
+      <svg className={css.icon} width="20" height="20">
+        <use href={icons + '#icon-help-circle'}></use>
+      </svg>
+      Need help?
+    </button>
     {isModalOpen && <NeedHelpModal isOpen={isModalOpen} closeModal={closeModal}/>}
   </div>
 }
