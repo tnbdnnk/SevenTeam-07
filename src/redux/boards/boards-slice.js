da3e6fcddb0952405791b24f892f1dc87596644c
GutMaria <gootmaria@gmail.com>
2024-05-14 14:42:21 +0300

    add card for column

--- src/redux/boards/boards-slice.js
+++ src/redux/boards/boards-slice.js
@@ -5,7 +5,8 @@ import {
   editBoard,
   fetchBoard,
   deleteBoard,
-  addColumn
+  addColumn,
+  addCard,
 } from './boards-operations';
 
 const initialState = {
@@ -68,9 +69,28 @@ const boardsSlice = createSlice({
       .addCase(addColumn.pending, pending)
       .addCase(addColumn.fulfilled, (state, { payload }) => {
         state.isLoading = false;
-        state.selectBoard.columns = [...state.selectBoard.columns, { ...payload, cards: [] }];
+        state.selectBoard.columns = [
+          ...state.selectBoard.columns,
+          { ...payload, cards: [] },
+        ];
       })
       .addCase(addColumn.rejected, rejected)
+      // addCard:
+      .addCase(addCard.pending, pending)
+      .addCase(addCard.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+        const { cardOwner } = payload;
+        const column = state.selectBoard.columns.find(
+          (col) => col._id === cardOwner
+        );
+        if (column) {
+          column.cards.push(payload);
+        } else {
+          console.error('Column not found for card owner:', cardOwner);
+        }
+        console.log('payload', payload);
+      })
+      .addCase(addCard.rejected, rejected);
   },
 });
 
