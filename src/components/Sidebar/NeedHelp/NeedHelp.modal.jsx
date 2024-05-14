9d726aece52c5942686b2db39ab37f4446631605
OleksiiHaidak <xalexey.g@gmail.com>
2024-05-14 18:44:58 +0300

    needhelp-backend commit

--- src/components/Sidebar/NeedHelp/NeedHelp.modal.jsx
+++ src/components/Sidebar/NeedHelp/NeedHelp.modal.jsx
@@ -1,12 +1,13 @@
 import Modal from "../../../helpers/ModalWindow/Modal";
 import css from "./need-help.module.css"
 import {
-  // useSelector,
+  useSelector,
   useDispatch
 } from 'react-redux';
 import { sendHelp } from "../../../redux/auth/auth-operations";
 // import { selectToken } from "../../../redux/auth/auth-selectors";
-
+import { selectUser } from "../../../redux/auth/auth-selectors";
+import toast from "react-hot-toast";
 
 const NeedHelpModal = ({ isOpen, closeModal }) => {
   const { theme } = useSelector(selectUser);
@@ -21,10 +22,18 @@ const NeedHelpModal = ({ isOpen, closeModal }) => {
 
       try {
         dispatch(sendHelp(formData));
-        console.log("Request sent successfully");
         closeModal();
+        toast.success(<div style={{ textAlign: 'center' }}>Message sent.<br/>We'll get back to you shortly</div>);
       } catch (error) {
         console.error("Error sending help request:", error.message);
+        toast.error('Ooops, there was an error...', {
+        style: {
+          border: '1px solid #713200',
+          padding: '10px',
+          color: '#713200',
+          fontWeight: 700,
+        },
+      });
       }
     };
 
@@ -34,22 +43,22 @@ const NeedHelpModal = ({ isOpen, closeModal }) => {
 
     return (
         <Modal className={css.needHelpBox} isOpen={isOpen} onClose={handleClose}>
-            <h3 className={css.title}>Need Help</h3>
+            <h3 className={[css.title, css[theme]].join(' ')}>Need Help</h3>
             <form onSubmit={handleSubmit}>
                 <input
-                    className={css.input}
+                    className={[css.input, css[theme]].join(' ')}
                     type="email"
                     name="email"
                     placeholder="Email address"
                     required
                 />
                 <textarea
-                    className={`${css.input} ${css.textarea}`}
+                    className={[css.input, css.textarea, css[theme]].join(' ')}
                     name="text"
                     placeholder="Comment"
                     required
                 ></textarea>
-                <button className={css.btnSend} type="submit">Send</button>
+                <button className={[css.btnSend, css[theme]].join(' ')} type="submit">Send</button>
             </form>
         </Modal>
     );
