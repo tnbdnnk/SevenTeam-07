c47894b057c2b6ec2d4c4fe23a3579a78e341901
AlMarina <almarina1017@gmail.com>
2024-05-15 17:26:41 +0300

    start

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -19,23 +19,21 @@ export const Header = ({ openBurger }) => {
         </svg>
       </button>
       <div className={css.wraperUser}>
-        {/* <div className={[css.wrapbtnTheme, css[theme]].join(' ')}>
-          Theme */}
         <button
-          className={[css.btnTheme, css.wrapbtnTheme, css[theme]].join(' ')}
+          className={[css.wrapbtnTheme, css[theme]].join(' ')}
           // className={css.btnTheme}
           onClick={() => setisThemeOpen((isThemeOpen) => !isThemeOpen)}
         >
           Theme
           <svg
-            //для 1го варіанту popup з перегортанням
+            //для варіанту popup з перегортанням іконки
             // className={
             //   isActive
             //     ? `${css.svgTheme} ${css.active} ${css[theme]}`
             //     : `${css.svgTheme} ${css[theme]}`
             // }
             className={`${css.svgTheme} ${css[theme]}`}
-            //для 1го варіанту popup з перегортанням
+            //для варіанту popup з перегортанням іконки
             // onClick={() => setisActive((isActive) => !isActive)}
             onClick={() => setisActive(true)}
           >
@@ -43,8 +41,7 @@ export const Header = ({ openBurger }) => {
           </svg>
           {isThemeOpen && <Theme />}
         </button>
-        {/* {isThemeOpen && <Theme />} */}
-        {/* </div> */}
+
         <UserInfo />
       </div>
     </header>
