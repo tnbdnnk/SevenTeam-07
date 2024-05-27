7f37b97905fd81627e4e9f0e88c15ffca99b8c05
AnnaFedorovaGit <ann.petrovska94@gmail.com>
2024-05-27 20:08:06 +0300

    make title and filter = fixed

--- src/components/ScreensPage/ScreensPage.jsx
+++ src/components/ScreensPage/ScreensPage.jsx
@@ -40,41 +40,17 @@ const ScreensPage = () => {
     } 
   }, [isLoading, boardList, boardName, navigate]); 
 
-  // const error = useSelector(selectError);
-
-  // import slug from 'slug';
-// const handleSelect = (boardId) => {
-//   // Здійснюємо запит на сервер, щоб отримати дані про борду за її ID
-//   fetch(/api/boards/${boardId})
-//     .then(response => response.json())
-//     .then(data => {
-//       if (data) {
-//         // Якщо борда знайдена, викликаємо dispatch з отриманими даними
-//         dispatch(selectBoard(data.id));
-//         // Створюємо slug для URL
-//         const boardSlug = slug(data.title);
-//         // Змінюємо шлях за допомогою navigate
-//         navigate(/home/${boardSlug}, { replace: true });
-//       } else {
-//         console.error(Борду з id ${boardId} не знайдено);
-//       }
-//     })
-//     .catch(error => {
-//       console.error('Помилка при отриманні даних про борду:', error);
-//     });
-// }
-
   return (
   <>
       {board && 
     <>
         <Background>
-          <section className={[css.section, css[theme]].join(' ')}>
-            <div className={css.activeSettingsWrapper}>
+          <div className={css.activeSettingsWrapper}>
               <h2 className={[css.title, css[theme]].join(' ')}>{board.title}</h2>
               <Filters />
-            </div>
-
+          </div>
+          
+          <section className={[css.section, css[theme]].join(' ')}>
             <div>
               <ul className={css.columnList}>
                 {board.columns && board.columns
